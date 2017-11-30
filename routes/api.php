df291f258a1f55b1cd7990bc5c8dc9611fdd440b
AleZek <zechini.alessandro@gmail.com>
2017-11-30 18:50:25 +0100

    logout

--- routes/api.php
+++ routes/api.php
@@ -19,8 +19,30 @@ use Illuminate\Http\Request;
 
 // Login
 Route::post('login', 'Auth\LoginController@login');
+// Logout
+Route::post('logout', 'Auth\LoginController@logout');
 
-Route::group(['middleware' => 'auth:api'], function () {
+
+// API protette
+//Route::group(['middleware' => 'auth:api'], function () {
+//
+//
+//// Standard API per users
+//Route::get('getutenti', 'UserController@getall');
+//Route::get('getutente/{id}', 'UserController@getById');
+//Route::get('utente/{id}/getindirizzo', 'UserController@getindirizzo');
+//Route::post('inserisci-utente', 'UserController@create');
+//Route::post('modifica-utente', 'UserController@update');
+//Route::post('elimina-utente', 'UserController@delete');
+//
+//// Standard API per indirizzo
+//Route::get('getindirizzi', 'IndirizzoController@getall');
+//Route::get('getindirizzo/{id}', 'IndirizzoController@getById');
+////Route::get('indirizzo/{id}/getrelated/{type}', 'IndirizzoController@getrelated');
+//Route::post('inserisci-indirizzo', 'IndirizzoController@create');
+//Route::post('modifica-indirizzo', 'IndirizzoController@update');
+//Route::post('elimina-indirizzo', 'IndirizzoController@delete');
+//});
 
 
 // Standard API per users
@@ -38,21 +60,3 @@ Route::get('getindirizzo/{id}', 'IndirizzoController@getById');
 Route::post('inserisci-indirizzo', 'IndirizzoController@create');
 Route::post('modifica-indirizzo', 'IndirizzoController@update');
 Route::post('elimina-indirizzo', 'IndirizzoController@delete');
-});
-
-//
-//// Standard API per users
-//Route::get('getutenti', 'UserController@getall');
-//Route::get('getutente/{id}', 'UserController@getById');
-//Route::get('utente/{id}/getindirizzo', 'UserController@getindirizzo');
-//Route::post('inserisci-utente', 'UserController@create');
-//Route::post('modifica-utente', 'UserController@update');
-//Route::post('elimina-utente', 'UserController@delete');
-//
-//// Standard API per indirizzo
-//Route::get('getindirizzi', 'IndirizzoController@getall');
-//Route::get('getindirizzo/{id}', 'IndirizzoController@getById');
-////Route::get('indirizzo/{id}/getrelated/{type}', 'IndirizzoController@getrelated');
-//Route::post('inserisci-indirizzo', 'IndirizzoController@create');
-//Route::post('modifica-indirizzo', 'IndirizzoController@update');
-//Route::post('elimina-indirizzo', 'IndirizzoController@delete');
